f8cc2b816eecb80b48fadc3b55b0864818a1301b
metalfabs <fabian.suceveanu@gmail.com>
2021-12-25 19:44:32 -0500

    Fix layer numbers
    Layer numbers incremented due to additional Colemak-DH layer.

--- keymap.c
+++ keymap.c
@@ -13,7 +13,7 @@ enum combos {
   ENT_BSPC_DEL
 };
 
-#define KC_ADJA LT(3,KC_A)
+#define KC_ADJA LT(4,KC_A)
 
 /* Home row mods for QWERTY */
 #define KC_ALTS LALT_T(KC_S)
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q,       KC_W,    KC_E,    KC_R, KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
 		KC_ADJA, KC_ALTS, KC_CTLD, KC_SFTF, KC_G,    KC_H, KC_SFTJ, KC_CTLK, KC_AGRL, KC_GUIQ,
 		KC_Z,       KC_X,    KC_C,    KC_V, KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                            LT(1,KC_ESC), KC_SPC,    KC_ENT, LT(2,KC_BSPC)),
+                            LT(2,KC_ESC), KC_SPC,    KC_ENT, LT(3,KC_BSPC)),
 
 	/* Colemak-DH
 	 * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
@@ -82,13 +82,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q,       KC_W,    KC_F,    KC_P, KC_B,    KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,
 		KC_ADJA, KC_ALTR, KC_CTLS, KC_SFTT, KC_G,    KC_M, KC_SFTN, KC_CTLE, KC_AGRI, KC_GUIO,
 		KC_Z,       KC_X,    KC_C,    KC_D, KC_V,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
-                            LT(1,KC_ESC), KC_SPC,    KC_ENT, LT(2,KC_BSPC)),
+                            LT(2,KC_ESC), KC_SPC,    KC_ENT, LT(3,KC_BSPC)),
 
 	 /* LOWER
 	 * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
 	 * │   1  │   2  │   3  │   4  │   5  │   6  │   7  │   8  │   9  │   0  │
 	 * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
-	 * │ TG 3 │ Alt  │ Ctrl │ Shft │      │ Left │ Down │  Up  │ Right│   ;  │
+	 * │ TG 4 │ Alt  │ Ctrl │ Shft │      │ Left │ Down │  Up  │ Right│   ;  │
 	 * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
 	 * │      │  {   │  [   │  (   │      │      │  )   │  ]   │  }   │  :   │
 	 * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_LOWER] = LAYOUT(
 		KC_1,       KC_2,    KC_3,    KC_4,    KC_5,     KC_6,       KC_7,    KC_8,    KC_9,    KC_0,
-		TG(3),   OSM_ALT, OSM_CTL, OSM_SFT, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, 
+		TG(4),   OSM_ALT, OSM_CTL, OSM_SFT, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, 
  		_______, KC_LCBR, KC_LBRC, KC_LPRN, _______,     _______, KC_RPRN, KC_RBRC, KC_RCBR, KC_COLN,
                                    _______, _______,     _______, _______),
 	
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_ADJUST] = LAYOUT(
 		_______, KC_HOME,   KC_UP,  KC_END, KC_PGUP,      KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,   RESET,
-		TG(3),   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,      KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
+		TG(4),   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,      KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
 		_______, _______, _______, _______,   DF(0),        DF(1), _______, KC_BTN3, _______, _______,
 		                           _______, _______,      _______, _______)
 };
