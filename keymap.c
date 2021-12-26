c716be5e0a1e50e1f5298e94cfb6ff8acea79f58
metalfabs <fabian.suceveanu@gmail.com>
2021-12-26 11:31:09 -0500

    Change layer names
    Dashes in enum are not allowed? Changed COLEMAK-DH to COLEMAKDH

--- keymap.c
+++ keymap.c
@@ -2,7 +2,7 @@
 
 enum ferris_layers {
   _QWERTY,
-  _COLEMAK-DH
+  _COLEMAKDH
   _LOWER,
   _RAISE,
   _ADJUST
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
 	 */
 
-	[_COLEMAK-DH] = LAYOUT(
+	[_COLEMAKDH] = LAYOUT(
 		KC_Q,       KC_W,    KC_F,    KC_P, KC_B,    KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,
 		KC_ADJA, KC_ALTR, KC_CTLS, KC_SFTT, KC_G,    KC_M, KC_SFTN, KC_CTLE, KC_AGRI, KC_GUIO,
 		KC_Z,       KC_X,    KC_C,    KC_D, KC_V,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
