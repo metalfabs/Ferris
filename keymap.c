1fb9c2a57277aba184cba37c549413b0158dab2e
metalfabs <fabian.suceveanu@gmail.com>
2022-02-10 08:04:14 -0500

    Swap space and enter

--- keymap.c
+++ keymap.c
@@ -9,8 +9,8 @@ enum ferris_layers {
 };
 
 enum combos {
-  ESC_SPC_TAB,
-  ENT_BSPC_DEL
+  ESC_ENT_TAB,
+  ENT_SPC_DEL
 };
 
 
@@ -39,12 +39,12 @@ enum combos {
 #define OSM_CTL OSM(MOD_LCTL)
 #define OSM_SFT OSM(MOD_LSFT)
 
-const uint16_t PROGMEM combtab[] = {LT(2,KC_ESC), LT(4,KC_SPC) COMBO_END};
-const uint16_t PROGMEM combdel[] = {KC_ENT, LT(3,KC_BSPC), COMBO_END};
+const uint16_t PROGMEM combtab[] = {LT(2,KC_ESC), LT(4,KC_ENT) COMBO_END};
+const uint16_t PROGMEM combdel[] = {KC_SPC, LT(3,KC_BSPC), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [ESC_SPC_TAB]  = COMBO(combtab, KC_TAB),
-    [ENT_BSPC_DEL] = COMBO(combdel, KC_DEL),
+    [ESC_ENT_TAB]  = COMBO(combtab, KC_TAB),
+    [SPC_BSPC_DEL] = COMBO(combdel, KC_DEL),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q,       KC_W,    KC_E,    KC_R, KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
 		KC_WINA, KC_ALTS, KC_CTLD, KC_SFTF, KC_G,    KC_H, KC_SFTJ, KC_CTLK, KC_AGRL, KC_GUIQ,
 		KC_Z,       KC_X,    KC_C,    KC_V, KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                            LT(2,KC_ESC),   LT(4,KC_SPC),    KC_ENT, LT(3,KC_BSPC)),
+                            LT(2,KC_ESC),   LT(4,KC_ENT),    KC_SPC, LT(3,KC_BSPC)),
 
 
 	[_COLEMAKDH] = LAYOUT(
 		KC_Q,       KC_W,    KC_F,    KC_P, KC_B,    KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,
 		KC_WINA, KC_ALTR, KC_CTLS, KC_SFTT, KC_G,    KC_M, KC_SFTN, KC_CTLE, KC_AGRI, KC_GUIO,
 		KC_Z,       KC_X,    KC_C,    KC_D, KC_V,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
-                              LT(2,KC_ESC), LT(4,KC_SPC),    KC_ENT, LT(3,KC_BSPC)),
+                              LT(2,KC_ESC), LT(4,KC_ENT),    KC_SPC, LT(3,KC_BSPC)),
 
 
 	[_NUMBER] = LAYOUT(
